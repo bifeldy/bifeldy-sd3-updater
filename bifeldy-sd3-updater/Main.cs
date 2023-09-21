3e56d464fa9319af18207c3bcd0bd10230d69acb
Bifeldy <bifeldy@gmail.com>
2023-09-22 00:25:44 +0900

    Add project files.

---
+++ bifeldy-sd3-updater/Main.cs
@@ -0,0 +1,76 @@
+﻿/**
+* 
+* Author       :: Basilius Bias Astho Christyono
+* Phone        :: (+62) 889 236 6466
+* 
+* Department   :: IT SD 03
+* Mail         :: bias@indomaret.co.id
+* 
+* Catatan      :: Window Utama
+* 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bifeldy_sd3_updater {
+
+    public sealed partial class CMain : Form {
+
+        private readonly string[] _args;
+
+        public CMain() {
+            InitializeComponent();
+        }
+
+        public CMain(string[] args) : this() {
+            _args = args;
+        }
+
+        private async void CMain_Load(object sender, EventArgs e) {
+            await LoadAllAplicationList();
+            if (_args.Length == 0) {
+                txtFilterSearch.ReadOnly = false;
+                dgvDaftarAplikasi.Enabled = true;
+                btnDownloadUpdate.Enabled = true;
+            }
+            else if (_args.Length > 2) {
+                MessageBox.Show("Invalid Arguments", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            else {
+                btnDownloadUpdate.Text = "Please Wait";
+                txtFilterSearch.Text = _args[0];
+                // TODO ::
+                BtnDownloadUpdate_Click(this, EventArgs.Empty);
+            }
+        }
+
+        private async void BtnDownloadUpdate_Click(object sender, EventArgs e) {
+            btnDownloadUpdate.Text = "Please Wait";
+            // TODO ::
+        }
+
+        private async Task LoadAllAplicationList() {
+            // TODO ::
+            await Task.Run(() => {
+                Thread.Sleep(5000);
+            });
+        }
+
+        private void TxtFilterSearch_TextChanged(object sender, EventArgs e) {
+            // TODO ::
+        }
+
+    }
+
+}
---
+++ bifeldy-sd3-updater/Program.cs
@@ -0,0 +1,50 @@
+﻿/**
+* 
+* Author       :: Basilius Bias Astho Christyono
+* Phone        :: (+62) 889 236 6466
+* 
+* Department   :: IT SD 03
+* Mail         :: bias@indomaret.co.id
+* 
+* Catatan      :: Entry Point
+* 
+*/
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace bifeldy_sd3_updater {
+
+    static class Program {
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [STAThread] // updater.exe app_name.exe pid
+        static void Main(string[] args) {
+            Process currentProcess = Process.GetCurrentProcess();
+            Process[] allProcess = Process.GetProcessesByName(currentProcess.ProcessName);
+            using (Mutex mutex = new Mutex(true, currentProcess.MainModule.ModuleName, out bool createdNew)) {
+                if (createdNew) {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new CMain(args));
+                }
+                else {
+                    foreach (Process process in allProcess) {
+                        if (process.Id != currentProcess.Id) {
+                            SetForegroundWindow(process.MainWindowHandle);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
