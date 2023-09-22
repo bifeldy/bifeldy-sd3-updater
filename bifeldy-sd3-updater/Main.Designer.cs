38744511995a3ecc5609b3225c31750c9c07bd87
B. Bias A. Ch <bias@indomaret.co.id>
2023-09-22 16:52:12 +0700

    Build Updater

--- bifeldy-sd3-updater/Main.Designer.cs
+++ bifeldy-sd3-updater/Main.Designer.cs
@@ -28,8 +28,8 @@ namespace bifeldy_sd3_updater {
         private void InitializeComponent() {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CMain));
             this.btnDownloadUpdate = new System.Windows.Forms.Button();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.label1 = new System.Windows.Forms.Label();
+            this.pgLoading = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
             this.dgvDaftarAplikasi = new System.Windows.Forms.DataGridView();
             this.txtFilterSearch = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,33 +40,34 @@ namespace bifeldy_sd3_updater {
             // 
             this.btnDownloadUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnDownloadUpdate.Enabled = false;
-            this.btnDownloadUpdate.Location = new System.Drawing.Point(272, 126);
+            this.btnDownloadUpdate.Location = new System.Drawing.Point(272, 107);
             this.btnDownloadUpdate.Name = "btnDownloadUpdate";
-            this.btnDownloadUpdate.Size = new System.Drawing.Size(100, 45);
+            this.btnDownloadUpdate.Size = new System.Drawing.Size(100, 40);
             this.btnDownloadUpdate.TabIndex = 0;
-            this.btnDownloadUpdate.Text = "Download Update";
+            this.btnDownloadUpdate.Text = "Please Wait ...";
             this.btnDownloadUpdate.UseVisualStyleBackColor = true;
             this.btnDownloadUpdate.Click += new System.EventHandler(this.BtnDownloadUpdate_Click);
             // 
-            // progressBar1
+            // pgLoading
             // 
-            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.pgLoading.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.progressBar1.Location = new System.Drawing.Point(18, 153);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(240, 10);
-            this.progressBar1.TabIndex = 1;
-            this.progressBar1.Value = 50;
+            this.pgLoading.Location = new System.Drawing.Point(18, 131);
+            this.pgLoading.Name = "pgLoading";
+            this.pgLoading.Size = new System.Drawing.Size(240, 10);
+            this.pgLoading.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pgLoading.TabIndex = 1;
+            this.pgLoading.Value = 50;
             // 
-            // label1
+            // lblStatus
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 137);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(177, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Loading All Available Applications ...";
+            this.lblStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(18, 115);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(177, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "Loading All Available Applications ...";
             // 
             // dgvDaftarAplikasi
             // 
@@ -75,24 +76,28 @@ namespace bifeldy_sd3_updater {
             this.dgvDaftarAplikasi.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDaftarAplikasi.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgvDaftarAplikasi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvDaftarAplikasi.Enabled = false;
             this.dgvDaftarAplikasi.Location = new System.Drawing.Point(12, 44);
+            this.dgvDaftarAplikasi.MultiSelect = false;
             this.dgvDaftarAplikasi.Name = "dgvDaftarAplikasi";
-            this.dgvDaftarAplikasi.ReadOnly = true;
-            this.dgvDaftarAplikasi.Size = new System.Drawing.Size(360, 71);
+            this.dgvDaftarAplikasi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvDaftarAplikasi.Size = new System.Drawing.Size(360, 55);
             this.dgvDaftarAplikasi.TabIndex = 4;
             // 
             // txtFilterSearch
             // 
             this.txtFilterSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilterSearch.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
             this.txtFilterSearch.Location = new System.Drawing.Point(115, 12);
             this.txtFilterSearch.Name = "txtFilterSearch";
             this.txtFilterSearch.ReadOnly = true;
             this.txtFilterSearch.Size = new System.Drawing.Size(257, 20);
             this.txtFilterSearch.TabIndex = 5;
-            this.txtFilterSearch.TextChanged += new System.EventHandler(this.TxtFilterSearch_TextChanged);
+            this.txtFilterSearch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtFilterSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtFilterSearch_KeyDown);
             // 
             // label2
             // 
@@ -108,13 +113,13 @@ namespace bifeldy_sd3_updater {
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(384, 183);
+            this.ClientSize = new System.Drawing.Size(384, 161);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtFilterSearch);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pgLoading);
             this.Controls.Add(this.btnDownloadUpdate);
             this.Controls.Add(this.dgvDaftarAplikasi);
+            this.Controls.Add(this.lblStatus);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -134,8 +139,8 @@ namespace bifeldy_sd3_updater {
         #endregion
 
         private System.Windows.Forms.Button btnDownloadUpdate;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar pgLoading;
+        private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.DataGridView dgvDaftarAplikasi;
         private System.Windows.Forms.TextBox txtFilterSearch;
         private System.Windows.Forms.Label label2;
--- bifeldy-sd3-updater/Main.cs
+++ bifeldy-sd3-updater/Main.cs
@@ -16,7 +16,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,51 +31,234 @@ namespace bifeldy_sd3_updater {
 
         private readonly string[] _args;
 
+        private readonly string UpdaterFtpIpDomain = "172.24.16.171";
+        private readonly string UpdaterFtpPort = "21";
+        private readonly string UpdaterFtpUsername = "sd3";
+        private readonly string UpdaterFtpPassword = "itsd3";
+
+        private readonly string UpdaterWorkDir = "Project_Postgresql/Hasil_PG";
+
+        private string ConnectionString = string.Empty;
+        List<string> Directories = new List<string>();
+
+        IProgress<string> statInfo = null;
+
         public CMain() {
             InitializeComponent();
         }
 
         public CMain(string[] args) : this() {
             _args = args;
+            // --
+            ConnectionString = $"ftp://{UpdaterFtpUsername}:{UpdaterFtpPassword}@{UpdaterFtpIpDomain}:{UpdaterFtpPort}/{UpdaterWorkDir}";
+
+            statInfo = new Progress<string>(txt => {
+                lblStatus.Text = txt;
+            });
         }
 
         private async void CMain_Load(object sender, EventArgs e) {
+            pgLoading.Value = 0;
+            pgLoading.Maximum = 1;
             await LoadAllAplicationList();
             if (_args.Length == 0) {
                 txtFilterSearch.ReadOnly = false;
                 dgvDaftarAplikasi.Enabled = true;
                 btnDownloadUpdate.Enabled = true;
+                btnDownloadUpdate.Text = "Download Or Update";
+                FilterSearchAllApps();
             }
             else if (_args.Length > 2) {
                 MessageBox.Show("Invalid Arguments", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
                 Application.Exit();
             }
             else {
-                btnDownloadUpdate.Text = "Please Wait";
-                txtFilterSearch.Text = _args[0];
-                // TODO ::
-                BtnDownloadUpdate_Click(this, EventArgs.Empty);
+                txtFilterSearch.Text = _args[0].ToUpper().Replace(".EXE", "");
+                txtFilterSearch.Refresh();
+                FilterSearchAllApps(false);
+                AutoDownloadAndInstall();
             }
         }
 
         private async void BtnDownloadUpdate_Click(object sender, EventArgs e) {
-            btnDownloadUpdate.Text = "Please Wait";
-            btnDownloadUpdate.Enabled = false;
-            // TODO ::
-            btnDownloadUpdate.Enabled = true;
-            btnDownloadUpdate.Text = "Download Update";
+            await DownloadAndInstall();
+        }
+
+        private void TxtFilterSearch_KeyDown(object sender, KeyEventArgs e) {
+            switch (e.KeyCode) {
+                case Keys.Enter:
+                    FilterSearchAllApps();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private async Task DownloadAndInstall() {
+            try {
+                string selectedFilePath = dgvDaftarAplikasi.SelectedCells[0].Value.ToString();
+                string downloadedFilePath = Path.Combine(Application.StartupPath, selectedFilePath);
+                pgLoading.Maximum += 3;
+                btnDownloadUpdate.Text = "Please Wait ...";
+                btnDownloadUpdate.Enabled = false;
+                using (WebClient request = new WebClient()) {
+                    request.DownloadProgressChanged += (s, e) => {
+                        lblStatus.Text = $"Downloading ... {e.BytesReceived} Bytes";
+                    };
+                    await request.DownloadFileTaskAsync(new Uri($"{ConnectionString}/{selectedFilePath}"), downloadedFilePath);
+                }
+                pgLoading.Value++;
+                if (_args.Length == 2) {
+                    pgLoading.Maximum++;
+                    lblStatus.Text = $"Killing Process '{_args[0]}' ...";
+                    await Task.Run(() => {
+                        try {
+                            Process proc = Process.GetProcessById(int.Parse(_args[1]));
+                            if (proc.ProcessName == _args[0]) {
+                                proc.Kill();
+                            }
+                        }
+                        catch {
+                            // Process Already Exited
+                        }
+                    });
+                    pgLoading.Value++;
+                }
+                await Task.Run(() => {
+                    using (FileStream fs = File.OpenRead(downloadedFilePath)) {
+                        using (ZipArchive za = new ZipArchive(fs)) {
+                            string parentFolder = string.Empty;
+                            for (int i = 0; i < za.Entries.Count; i++) {
+                                if (za.Entries[i].FullName.EndsWith("/")) {
+                                    if (string.IsNullOrEmpty(parentFolder) && i == 0) {
+                                        parentFolder = za.Entries[i].FullName.Split('/').First() + "/";
+                                    }
+                                    else if (za.Entries[i].FullName.Length < parentFolder.Length) {
+                                        parentFolder = za.Entries[i].FullName;
+                                    }
+                                }
+                            }
+                            for (int i = 0; i < za.Entries.Count; i++) {
+                                string name = za.Entries[i].FullName;
+                                if (!string.IsNullOrEmpty(name) && !name.StartsWith(parentFolder)) {
+                                    parentFolder = string.Empty;
+                                    break;
+                                }
+                                else if (name.StartsWith(parentFolder + "Release/") || name.StartsWith(parentFolder + "Debug/")) {
+                                    parentFolder = name;
+                                    break;
+                                }
+                            }
+                            string extractPrefix = parentFolder;
+                            foreach (ZipArchiveEntry zae in za.Entries) {
+                                if (zae.FullName.StartsWith(extractPrefix)) {
+                                    string name = zae.FullName;
+                                    if (!string.IsNullOrEmpty(extractPrefix)) {
+                                        name = zae.FullName.Replace(extractPrefix, "");
+                                    }
+                                    string path = Path.Combine(Application.StartupPath, name);
+                                    if (string.IsNullOrEmpty(zae.Name)) {
+                                        if (!Directory.Exists(path)) {
+                                            Directory.CreateDirectory(path);
+                                        }
+                                    }
+                                    else {
+                                        statInfo.Report($"Extracting '{zae.Name}' ...");
+                                        zae.ExtractToFile(path, true);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    File.Delete(downloadedFilePath);
+                });
+                pgLoading.Value++;
+                try {
+                    Process proc = null;
+                    if (_args.Length == 2) {
+                        statInfo.Report($"Re-Launching '{_args[0]}' ...");
+                        proc = Process.Start(Path.Combine(Application.StartupPath, $"{_args[0]}"));
+                    }
+                    else {
+                        string[] directories = Directory.GetFiles(Application.StartupPath);
+                        foreach (string d in directories) {
+                            if (d.ToUpper().EndsWith(".EXE") && !d.ToUpper().EndsWith($"{Application.ProductName}.EXE".ToUpper())) {
+                                string path = Path.Combine(Application.StartupPath, d);
+                                proc = Process.Start(path);
+                            }
+                        }
+                    }
+                    if (proc == null) {
+                        throw new Exception("Executable File Not Found");
+                    }
+                }
+                catch (Exception e) {
+                    Process.Start("explorer.exe", Application.StartupPath);
+                }
+                pgLoading.Value++;
+                lblStatus.Text = "Download Update Completed ...";
+                btnDownloadUpdate.Enabled = true;
+                btnDownloadUpdate.Text = "Download Or Update";
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private async Task LoadAllAplicationList() {
-            // TODO ::
-            await Task.Run(() => {
-                // TODO ::
-                Thread.Sleep(5000);
-            });
+            lblStatus.Text = "Getting List ...";
+            Directories.Clear();
+            try {
+                await Task.Run(() => {
+                    FtpWebRequest ftpRequest = (FtpWebRequest) WebRequest.Create(new Uri(ConnectionString));
+                    ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+                    FtpWebResponse response = (FtpWebResponse) ftpRequest.GetResponse();
+                    using (StreamReader streamReader = new StreamReader(response.GetResponseStream())) {
+                        string line = streamReader.ReadLine();
+                        while (!string.IsNullOrEmpty(line)) {
+                            Directories.Add(line);
+                            line = streamReader.ReadLine();
+                        }
+                    }
+                });
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            pgLoading.Value++;
+            lblStatus.Text = "Loading Completed ...";
         }
 
-        private void TxtFilterSearch_TextChanged(object sender, EventArgs e) {
-            // TODO ::
+        private void FilterSearchAllApps(bool enableAfterLoaded = true) {
+            dgvDaftarAplikasi.Enabled = false;
+            dgvDaftarAplikasi.Rows.Clear();
+            dgvDaftarAplikasi.Columns.Clear();
+            dgvDaftarAplikasi.Columns.Add(new DataGridViewTextBoxColumn {
+                Name = "filepath",
+                HeaderText = "File Name",
+                ReadOnly = true,
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+            });
+            foreach (string dir in Directories.OrderByDescending(d => d.ToUpper())) {
+                string d = dir.Replace($"{UpdaterWorkDir.Split('/').Last()}/", "");
+                if (d.ToUpper().StartsWith(txtFilterSearch.Text.ToUpper()) && d.ToUpper().EndsWith(".ZIP")) {
+                    dgvDaftarAplikasi.Rows.Add(d);
+                }
+            }
+            dgvDaftarAplikasi.Refresh();
+            if (enableAfterLoaded) {
+                dgvDaftarAplikasi.Enabled = true;
+            }
+        }
+
+        private async void AutoDownloadAndInstall() {
+            if (dgvDaftarAplikasi.Rows.Count == 0) {
+                MessageBox.Show("No Update Available", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+                await DownloadAndInstall();
+            }
+            Application.Exit();
         }
 
     }
